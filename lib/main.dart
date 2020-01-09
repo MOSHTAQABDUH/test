1ca3183c2b37ee90a495fd552906969cbdbd85bd
MOSHTAQABDUH <aklan1992msh@gmail.com>
2020-01-09 09:31:56 +0300

    test

---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'data_table_demo.dart';
+import 'pages/custom_column_nested_table.dart';
+import 'pages/custom_column_table.dart';
+import 'pages/simple_table.dart';
+import 'homepage.dart';
+import 'easyweb.dart';
+void enablePlatformOverrideForDesktop() {
+  if (!kIsWeb && (Platform.isMacOS || Platform.isWindows || Platform.isLinux)) {
+    debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
+  }
+}
+
+void main() {
+  enablePlatformOverrideForDesktop();
+
+  runApp(MyApp());
+}
+//void main() => runApp(new HomePage());
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Json Table Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen (),
+    );
+  }
+}
+
+class RootPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Table Widget"),
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(
+                text: "Simple Table",
+              ),
+              Tab(
+                text: "Custom Table",
+              ),
+              Tab(
+                text: "Nested Data Table",
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            SimpleTable(),
+            CustomColumnTable(),
+            CustomColumnNestedTable(),
+          ],
+        ),
+      ),
+    );
+  }
+}
